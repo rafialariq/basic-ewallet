1f16b3774495e716185614eef3611d3ba5405fda
rafi alariq <rmalariq@gmail.com>
2023-04-27 15:37:52 +0700

    history feature

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ func main() {
 	dbName := utils.DotEnv("DB_NAME")
 	sslMode := utils.DotEnv("SSL_MODE")
 	serverPort := utils.DotEnv("SERVER_PORT")
-	baseFilePath := utils.DotEnv("BASE_FILE_PATH")
 
 	dataSourceName := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", dbUser, dbPassword, dbHost, dbPort, dbName, sslMode)
 	db, err := sqlx.Connect("postgres", dataSourceName)
@@ -40,28 +39,6 @@ func main() {
 		log.Println("Connected to DB")
 	}
 
-	fileRepo := repository.NewFileRepository(baseFilePath)
-
-	userRepo := repository.NewUserRepo(db)
-	userUsecase := usecase.NewUserUsecase(userRepo, fileRepo)
-	userController := controller.NewUserController(userUsecase)
-
-	transferRepo := repository.NewTransferRepo(db)
-	transferUsecase := usecase.NewTransferUsecase(transferRepo)
-	transferController := controller.NewTransferController(transferUsecase, userUsecase)
-
-	topUpRepo := repository.NewTopUpRepo(db)
-	topUpUsecase := usecase.NewTopUpUsecase(topUpRepo)
-	topUpController := controller.NewTopUpController(topUpUsecase, userUsecase)
-
-	registerRepo := repository.NewRegisterRepo(db)
-	registerService := usecase.NewRegisterService(registerRepo)
-	registerController := controller.NewRegisterController(registerService)
-
-	loginRepo := repository.NewLoginRepo(db)
-	loginService := usecase.NewLoginService(loginRepo)
-	loginController := controller.NewLoginController(loginService)
-
 	historyRepo := repository.NewHistoryRepo(db)
 	historyUsecase := usecase.NewHistoryUsecase(historyRepo)
 	historyController := controller.NewHistoryController(historyUsecase)
@@ -71,21 +48,10 @@ func main() {
 	menuRouter := router.Group("/menu")
 	menuRouter.Use(middleware.AuthMiddleware())
 
-	menuRouter.GET("/profile/:username", userController.CheckProfile)
-	menuRouter.POST("/profile/edit", userController.EditProfile)
-	menuRouter.POST("/profile/edit/photo/:username", userController.EditPhotoProfile)
-	menuRouter.DELETE("/profile/:username", userController.UnregProfile)
-	menuRouter.POST("/transfer/bank", transferController.WithdrawBalance)
-	menuRouter.POST("/transfer/user", transferController.TransferBalance)
-	menuRouter.POST("/topup", topUpController.TopUpBalance)
-
 	menuRouter.GET("/history", historyController.FindAllByUser)
 	menuRouter.GET("/history/account/:accountTypeId", historyController.FindByAccountType)
 	menuRouter.GET("/history/type/:typeId", historyController.FindByType)
 	menuRouter.GET("/history/:more_than/:less_than", historyController.FindByAmount)
-
-	router.POST("/signup", registerController.RegisterHandler)
-	router.POST("/login", loginController.LoginHandler)
 
 	if err := router.Run(serverPort); err != nil {
 		log.Fatal(err)
